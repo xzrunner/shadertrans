e6503b4f7446861e5b9387e82e475e9416feeb0d
zz <xzrunner@gmail.com>
2021-01-10 08:10:33 +0800

    update 3rd

--- source/ShaderTrans.cpp
+++ source/ShaderTrans.cpp
@@ -4,10 +4,10 @@
 #include "shadertrans/GLSLangAdapter.h"
 
 #include <glslang/public/ShaderLang.h>
-#include <glslang/include/StandAlone/DirStackFileIncluder.h>
-#include <glslang/include/SPIRV/GlslangToSpv.h>
-#include <spirv-cross/spirv.hpp>
-#include <spirv-cross/spirv_glsl.hpp>
+#include <StandAlone/DirStackFileIncluder.h>
+#include <SPIRV/GlslangToSpv.h>
+#include <spirv.hpp>
+#include <spirv_glsl.hpp>
 #include <dxc/Support/WinIncludes.h>
 #include <dxc/dxcapi.h>
 
@@ -156,7 +156,7 @@ const char* entryPoint = "main";
 namespace shadertrans
 {
 
-void ShaderTrans::HLSL2SpirV(ShaderStage stage, const std::string& hlsl, 
+void ShaderTrans::HLSL2SpirV(ShaderStage stage, const std::string& hlsl,
                              std::vector<unsigned int>& spirv, std::ostream& out)
 {
     std::vector<const wchar_t*> dxcArgs;
@@ -175,6 +175,7 @@ void ShaderTrans::HLSL2SpirV(ShaderStage stage, const std::string& hlsl,
     std::wstring shaderProfile = hlsl_shader_profile_name(stage, 6, 0);;
 
     std::vector<DxcDefine> dxcDefines;
+
     //CComPtr<IDxcIncludeHandler> includeHandler = new hlsl::ScIncludeHandler(std::move(hlsl::DefaultLoadCallback));
     CComPtr<IDxcIncludeHandler> includeHandler = nullptr;
     CComPtr<IDxcOperationResult> compileResult;
@@ -211,7 +212,7 @@ void ShaderTrans::HLSL2SpirV(ShaderStage stage, const std::string& hlsl,
     }
 }
 
-void ShaderTrans::GLSL2SpirV(ShaderStage stage, const std::string& glsl, 
+void ShaderTrans::GLSL2SpirV(ShaderStage stage, const std::string& glsl,
 	                         std::vector<unsigned int>& spirv, std::ostream& out)
 {
     GLSLangAdapter::Instance()->Init();
@@ -222,7 +223,7 @@ void ShaderTrans::GLSL2SpirV(ShaderStage stage, const std::string& glsl,
     glslang::TShader shader(shader_type);
     const char* src_cstr = glsl.c_str();
     shader.setStrings(&src_cstr, 1);
-    
+
     int client_input_semantics_version = 100; // maps to, say, #define VULKAN 100
     glslang::EShTargetClientVersion VulkanClientVersion = glslang::EShTargetVulkan_1_0;
     glslang::EShTargetLanguageVersion TargetVersion = glslang::EShTargetSpv_1_0;
@@ -247,7 +248,7 @@ void ShaderTrans::GLSL2SpirV(ShaderStage stage, const std::string& glsl,
     //includer.pushExternalLocalDirectory(Path);
 
     std::string preprocessed_glsl;
-    if (!shader.preprocess(&resources, default_version, ENoProfile, false, false, messages, &preprocessed_glsl, includer)) 
+    if (!shader.preprocess(&resources, default_version, ENoProfile, false, false, messages, &preprocessed_glsl, includer))
     {
         out << "GLSL Preprocessing Failed for: " << glsl << "\n";
         out << shader.getInfoLog() << "\n" << shader.getInfoDebugLog() << "\n";
@@ -278,7 +279,7 @@ void ShaderTrans::GLSL2SpirV(ShaderStage stage, const std::string& glsl,
     glslang::GlslangToSpv(*program.getIntermediate(shader_type), spirv, &logger, &spv_options);
 }
 
-void ShaderTrans::SpirV2GLSL(ShaderStage stage, const std::vector<unsigned int>& spirv, 
+void ShaderTrans::SpirV2GLSL(ShaderStage stage, const std::vector<unsigned int>& spirv,
                              std::string& glsl, std::ostream& out)
 {
     try {
